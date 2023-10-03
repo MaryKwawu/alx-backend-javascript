6fe53ca4c6c9347b647303897401349f844a2411
MaryKwawu <may419316@gmail.com>
2023-10-03 22:21:51 +0000

    added files

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,9 +1,3 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw Error('Position outside range');
-  }
-  const newBuffer = new ArrayBuffer(length);
-  const int8 = new Int8Array(newBuffer, 0, length);
-  int8.set([value], position);
-  return new DataView(newBuffer);
+export default function setFromArray(list) {
+  return new Set(list);
 }
